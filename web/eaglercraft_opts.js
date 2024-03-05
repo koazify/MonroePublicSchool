39803c4c0473d882ce7a038ff21c7ebde27ebcb7
koazify <149215835+koazify@users.noreply.github.com>
2024-03-05 21:01:47 +0000

    Idk

--- web/eaglercraft_opts.js
+++ web/eaglercraft_opts.js
@@ -3,7 +3,8 @@ window.eaglercraftXOpts = () => ({
   // joinServer: "ws" + window.location.href.slice(4),
   // assetsURI: "custom.epk",
 	servers: [
-		{ addr: "ws" + window.location.href.slice(4), name: "EaglercraftX Server" }
+		{ addr: "ws" + window.location.href.slice(4), name: "EaglercraftX Server" },
+		{ addr: "wss://otherserveraddress.here", name: "EaglercraftX Server"}
 	]
 });
 // document.title = "My Original Server Name:tm:";
